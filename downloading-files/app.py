ece1e0905fad004a4371302a6e4dd9b12ba601bd
Delink-D <delinkdesigns@gmail.com>
2017-08-10 19:32:18 +0300

    Convert the data into a string and split data by line

--- downloading-files/app.py
+++ downloading-files/app.py
@@ -10,3 +10,5 @@ file_url = input()
 def download_file(file_url):
     res = request.urlopen(file_url)
     file = res.read()       # read the data
+    file_txt = str(file)    # convert the data to string
+    lines = file_txt.split('\n')
