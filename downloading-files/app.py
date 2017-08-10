f9ff3b29702d6ec84a7a64419b0cae8adbe92e7e
Delink-D <delinkdesigns@gmail.com>
2017-08-10 19:24:30 +0300

    Downloading files from the web

---
+++ downloading-files/app.py
@@ -0,0 +1,9 @@
+from urllib import request
+
+# downloading files from the web using the url
+print('Input the file url o download..')
+print('--------------------------------')
+
+file_url = input()
+
+print(file_url)
