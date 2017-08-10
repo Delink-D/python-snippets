c97e5c68611d19d268bc9d2b7932c3275a19a800
Delink-D <delinkdesigns@gmail.com>
2017-08-10 19:38:55 +0300

    Create a file in a local machine, then write the read data into the file using a for loop

--- downloading-files/app.py
+++ downloading-files/app.py
@@ -9,6 +9,15 @@ file_url = input()
 
 def download_file(file_url):
     res = request.urlopen(file_url)
-    file = res.read()       # read the data
-    file_txt = str(file)    # convert the data to string
+    file = res.read()               # read the data
+    file_txt = str(file)            # convert the data to string
     lines = file_txt.split('\n')
+    location = r'file.txt'          # give the save file a name
+
+    # read and write to file
+    fx = open(location, 'w')
+
+    # a loop to write to the file line by line
+    for line in lines:
+        fx.write(line + '\n')
+    fx.close()
