51beccd31e8bdda8fbc8ed91c8e61f8b2bf8d070
Delink-D <delinkdesigns@gmail.com>
2017-08-19 10:09:31 +0300

    Adding a while loop
    looping through the url pages provided number of pages to crawl

--- web-crawler/app.py
+++ web-crawler/app.py
@@ -6,4 +6,8 @@ from bs4 import BeautifulSoup
 def spider(max_pages):
     pages = 1               # set the page nuber to crawl
 
+    while pages <= max_pages:
+        url = 'https://thenewboston.com/forum/recent_activity.php?page=' + str(pages)
+        source_code = requests.get(url)
+
 spider(1)
