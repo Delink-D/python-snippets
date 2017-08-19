d67720222ea6157febfd03af98bb471e9ddeb98a
Delink-D <delinkdesigns@gmail.com>
2017-08-19 10:03:07 +0300

    creating web craeler
    set up of the file structure

---
+++ web-crawler/app.py
@@ -0,0 +1,9 @@
+# a program that crawls the web web or info
+import requests
+from bs4 import BeautifulSoup
+
+
+def spider(max_pages):
+    pages = 1               # set the page nuber to crawl
+
+spider(1)
