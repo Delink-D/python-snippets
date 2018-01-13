c1c4dd2cd7de54ae9908fb50e6aa756a41e4d730
Delink-D <delinkdesigns@gmail.com>
2018-01-13 20:49:30 +0300

    added an input to the read write to file

--- write-to-file/app.py
+++ write-to-file/app.py
@@ -1,14 +1,19 @@
 # writing files with python
-print('Starting the write process to the file....')
+print('-----------------------------')
+print('Writing to file in python....')
+print('-----------------------------')
+
+# input to print
+inputText = input('What do you want to write to the file? ')
 fw = open('first.txt', 'w')  # create a file with name first.txt
-fw.write("This is my first sentence in writing files \n")  # write first sentence
-fw.write("Delink is cool \n")
+fw.write(inputText + '\n')  # write first sentence
+fw.write('\n<<<Delink is cool>>\n')
 
 fw.close()
 
 # reading files in python
 fr = open('first.txt', 'r')  # open the file
-text = fr.read()
+readText = fr.read()
 
-print(text)  # print the files content
+print(readText)  # print the files content
 fr.close()
